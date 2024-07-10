7390de9c67877becfb08ae464eeee98b1c2eb5f6
“Saranya” <saranyapsankar@gmail.com>
2024-07-10 23:14:22 +0530

    updated media query based style

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import WelcomeContent from './components/WelcomeContent'
 const App = () => {
   const [showContent, setShowContent] = useState(false)
   return (
-    <div className='font-display bg-slate-800 text-white'>
+    <div className='font-display bg-slate-800 text-white max-lg:p-10 overflow-x-hidden'>
       {/* {!showContent && <WelcomeContent setShowContent={setShowContent}/>} */}
       {!showContent &&  <HeaderSection />}
     </div>
